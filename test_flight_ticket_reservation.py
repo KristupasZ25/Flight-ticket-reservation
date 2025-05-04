bc246d91a80593c3791dabc7a2743c59ed4b1052
Kristupas <kristupaszazeckis@gmail.com>
2025-05-04 21:59:24 +0300

    first commit

--- test_flight_ticket_reservation.py
+++ test_flight_ticket_reservation.py
@@ -1,14 +1,14 @@
 import unittest
-from flight_ticket_reservation import User, Flight, Ticket, FlightFactory, generate_ticket_price
+from flight_ticket_reservation import User, Flight, Ticket, FlightFactory, generate_ticket_price, TICKET_CLASSES
 
 class TestUser(unittest.TestCase):
     def test_login_valid_user(self):
         user = User("KristupasZ", "kursinis", "kristupaszazeckis@gmail.com")
-        self.assertTrue(user.login("KristupasZ", "kursinis"))
+        self.assertIsNotNone(user.login("KristupasZ", "kursinis"))
 
     def test_login_invalid_user(self):
         user = User("KristupasZ", "kursinis", "kristupaszazeckis@gmail.com")
-        self.assertFalse(user.login("invalid_username", "invalid_password"))
+        self.assertIsNone(user.login("invalid_username", "invalid_password"))
 
 class TestFlightAndTicket(unittest.TestCase):
     def test_generate_ticket_price(self):
@@ -17,30 +17,42 @@ class TestFlightAndTicket(unittest.TestCase):
 
     def test_ticket_calculate_price_economy(self):
         flight = Flight("Vilnius", "London")
-        ticket = Ticket(flight, "KristupasZ", "economy", 1)
+        ticket = Ticket(flight, {"username": "KristupasZ"}, "economy", 1)
         self.assertEqual(ticket.calculate_ticket_price(), 100)
 
     def test_ticket_calculate_price_business(self):
         flight = Flight("Vilnius", "London")
-        ticket = Ticket(flight, "KristupasZ", "business", 1)
+        ticket = Ticket(flight, {"username": "KristupasZ"}, "business", 1)
         self.assertEqual(ticket.calculate_ticket_price(), 150)
 
     def test_ticket_calculate_price_first(self):
         flight = Flight("Vilnius", "London")
-        ticket = Ticket(flight, "KristupasZ", "first", 1)
+        ticket = Ticket(flight, {"username": "KristupasZ"}, "first", 1)
         self.assertEqual(ticket.calculate_ticket_price(), 200)
 
     def test_ticket_multiple_quantity(self):
         flight = Flight("Vilnius", "London")
-        ticket = Ticket(flight, "KristupasZ", "first", 2)
+        ticket = Ticket(flight, {"username": "KristupasZ"}, "first", 2)
         self.assertEqual(ticket.calculate_ticket_price(), 400)
 
 class TestFlightFactory(unittest.TestCase):
     def test_create_flight(self):
         factory = FlightFactory()
-        flight = factory.create_flight("Vilnius", "London")
+        flight = factory.create_flight("Vilnius", "London", "economy")
         self.assertEqual(flight.get_flight_info(), "Vilnius to London")
         self.assertEqual(flight.get_ticket_price(), 100)
+
+    def test_create_business_flight(self):
+        factory = FlightFactory()
+        flight = factory.create_flight("Vilnius", "London", "business")
+        self.assertEqual(flight.get_flight_info(), "Vilnius to London")
+        self.assertEqual(flight.get_ticket_price(), 150)
+
+    def test_create_first_class_flight(self):
+        factory = FlightFactory()
+        flight = factory.create_flight("Vilnius", "London", "first")
+        self.assertEqual(flight.get_flight_info(), "Vilnius to London")
+        self.assertEqual(flight.get_ticket_price(), 200)
 
 class TestGenerateTicketPrice(unittest.TestCase):
     def test_generate_ticket_price(self):
